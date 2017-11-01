2576fe5f0f654bb269b07b86deb5df313574e339
condor0010 <liatmp3@gmail.com>
2017-10-31 22:01:32 -0400

    Add files via upload

--- Program3_10.java
+++ Program3_10.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 public class Program3_10 {
 	public static void main(String[] args) {
 		Scanner Reader = new Scanner (System.in);
-		String name,loop;
-		int x,BOB,v;
-		System.out.println("hello what is your name? ");
+		String name;
+		int x,BOB;
+		System.out.print("hello what is your name? ");
 		name=Reader.nextLine();
 		do {
-		System.out.println("ok "+name+ " how may bottles of beer are on the wall? ");
+		System.out.print("ok "+name+ " how may bottles of beer are on the wall? ");
 		for (BOB=Reader.nextInt(); BOB>0; BOB--) {
-			System.out.println(BOB +" bottles of beer on the wall");
-			System.out.println(BOB +" beer on the wall");
+			int LO=BOB-1;
+			System.out.println(BOB +" bottles of beer on the wall, "+BOB +" beer on the wall");
+			System.out.println("Take one down and pass it around, " +LO+" bottles of beer on the wall.");
 		}
-		System.out.println("do yo want to go agen (Y or N)");
 	}
-		while ( BOB == 0 );
+		while (BOB==0);
 }
+
 }
